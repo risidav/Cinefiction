93561733f146ad31d7de061c4fd8438ba58f8aff
risidav <reidavi.26@gmail.com>
2024-08-22 11:41:24 -0300

    teste de commit2

---
+++ app/_layout.js
@@ -0,0 +1,28 @@
+import { Stack } from "expo-router";
+
+/*
+vermelho: #D93250
+vinho: #732743
+preto: #030404
+#030410
+branco: #F2F2F2
+*/ 
+
+export default function RootLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#030410",
+        },
+        headerTintColor: "#F2F2F2",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+        headerTitleAlign: "center", 
+      }}
+    >
+      <Stack.Screen name="index" options={{ title: "Cinefiction" }} />
+    </Stack>
+  );
+}
---
+++ app/index.js
@@ -0,0 +1,40 @@
+import { Pressable, Text, View, StyleSheet } from "react-native";
+
+/*
+vermelho: #D93250
+vinho: #732743
+preto: #030F26
+#030410
+branco: #F2F2F2
+*/ 
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Pressable style={styles.btentrar}>Entrar</Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:
+  {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F2F2F2"
+  },
+  btentrar:
+  {
+    paddingHorizontal: 90,
+    paddingVertical: 10,
+    borderRadius: 50,
+    backgroundColor: "#d93250",
+    color: "#f2f2f2",
+    fontSize: 25,
+    fontFamily: "sans-serif",
+    marginTop: "auto",
+    marginBottom: 55,
+  }
+});
